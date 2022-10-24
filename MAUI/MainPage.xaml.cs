ecf12adbabdb729536a3b267bcbe95e38ae0541d
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-10-25 00:06:01 +0530

    Add project files.

---
+++ MAUI/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUI;
+
+public partial class MainPage : TabbedPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Web/Program.cs
@@ -0,0 +1,14 @@
+using Common.Data;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Web;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSingleton<WeatherForecastService>();
+
+await builder.Build().RunAsync();
